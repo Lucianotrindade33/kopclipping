c3ad99ef605886c5fe15b269b7759683c9de0a31
Luciano Trindade <lucianotrindade.ti@gmail.com>
2017-06-26 20:40:50 -0300

    primeiro commit

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
+
 var TwitterPackage = require('twitter');
 
 var secret = {
-  consumer_key: 'YOURS',
-  consumer_secret: 'YOURS',
-  access_token_key: 'YOURS',
-  access_token_secret: 'YOURS'
+  consumer_key: 'CcM9BBYrrOuYqmjySrU5UWCpY',
+  consumer_secret:'W6GbiuOYeCPhAjpO5WnMacYtdrCS5bYi1WoxYYttmKPSOFXTzx',
+  access_token_key: '562442359-6LQr1crym0AYMChOEQoD4FcUFdBkDZ54gTcewgCQ',
+  access_token_secret: 'DgT99GwZdedXCKKDg84kDTF6xCzfrG8P49GWTBEPcv69e'
 }
 
 var Twitter = new TwitterPackage(secret);
 
-Twitter.stream('statuses/filter', {track: '@kopzinksi'}, function(stream) {
+Twitter.stream('statuses/filter', {track: '@G1'}, function(stream) {
   stream.on('data', function(tweet) {
     console.log(tweet.text);
   });
