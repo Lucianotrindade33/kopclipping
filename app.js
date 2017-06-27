d7d67510573ab29aba1ceb3dd89f92027d34575c
Luciano Trindade <lucianotrindade.ti@gmail.com>
2017-06-26 22:26:12 -0300

    terceiro commit

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 
 var TwitterPackage = require('twitter');
 
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
 var secret = {
   consumer_key: 'CcM9BBYrrOuYqmjySrU5UWCpY',
   consumer_secret:'W6GbiuOYeCPhAjpO5WnMacYtdrCS5bYi1WoxYYttmKPSOFXTzx',
@@ -8,14 +12,31 @@ var secret = {
   access_token_secret: 'DgT99GwZdedXCKKDg84kDTF6xCzfrG8P49GWTBEPcv69e'
 }
 
+//criando modelos
+
+var Post = mongoose.model('post', { mensagem: String });
+
 var Twitter = new TwitterPackage(secret);
 
-Twitter.stream('statuses/filter', {track: '@G1'}, function(stream) {
+Twitter.stream('statuses/filter', {track: '@jairbolsonaro'}, function(stream) {
   stream.on('data', function(tweet) {
     console.log(tweet.text);
+
+    var postAtual = new Post({mensagem: tweet.text});
+
+    postAtual.save(function(err){
+            if (err) {
+          console.log(err);
+        } else {
+          console.log('salvo com sucesso');
+        }
+          
+    });
+    //salvar n
   });
 
   stream.on('error', function(error) {
     console.log(error);
   });
 });
+
